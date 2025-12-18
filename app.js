d5a8aaf942afcda0d862d2bc0c4f89f37d6da829
meenatchi-sundaram-git <meenatchisundaram365@gmail.com>
2025-12-18 09:21:09 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req,res)=>{
+  res.end("Hello World from Azure DevOps!");
+});
+
+server.listen(port,()=>console.log("Running…"));
+
